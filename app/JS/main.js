4f2e1d8390a65a8f4184fd0bdc0c23a260a62af4
mellowed24 <eyada5@nycstudents.net>
2024-11-18 11:45:46 -0500

    Update main.js

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,5 +1,33 @@
 import "/css/style.css";
 
-const response = await fetch(
-  "https://eldenring.fanapis.com/api/bosses?limit=100"
-);
+
+async function getData() {
+  try {
+    const response = await fetch("https://eldenring.fanapis.com/api/bosses?limit=100"); 
+    const result = await response.json(); 
+
+    const filteredBosses = result.data.filter(boss => boss.name.startsWith("M")).forEach(boss => {
+      console.log(boss.name)
+    });
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+getData();
+function CreateCards(arr) {
+  clear()
+  arr.forEach((game) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeEnd",
+      `<div class="card">
+        <h3>${game.name}</h3>
+        <img src="${game.img}" alt="">
+        <h3 class="card-price"></h3>
+        <h3 class="card-rating">Rating: </h3>
+      </div>`
+    )
+  );
+}
+CreateCards(result);
