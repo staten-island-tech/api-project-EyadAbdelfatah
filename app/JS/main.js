b155f4a90dc34d0016cf157bd70a4cc87afc9fac
mellowed24 <eyada5@nycstudents.net>
2024-11-18 17:54:38 -0500

    n

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,15 +1,24 @@
 import "/css/style.css";
-
+const DOMSelectors = {
+  container: document.querySelector(".container")
+}
+let result;
 async function getData() {
   try {
     const response = await fetch(
       "https://eldenring.fanapis.com/api/bosses?limit=100"
     );
-    const result = await response.json();
+    result = await response.json();
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-}
 
+}
 getData();
-console.log(result);
+async function makeCards(array){
+  await getData();
+  array.forEach(boss => {
+    DOMSelectors.container.insertAdjacentHTML("beforeend",`<h3>${boss.name}</h3>`)
+  });
+}
+makeCards(result);
