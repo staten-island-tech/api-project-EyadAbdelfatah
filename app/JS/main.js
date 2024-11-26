50627aadcddc16177211eeb4bf7ea605e2642be1
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2024-11-26 14:00:10 -0500

    hkl'

--- app/JS/main.js
+++ app/JS/main.js
@@ -18,14 +18,13 @@ async function makeCards() {
   await getData();
 
   result.data.forEach((boss) => {
-    if (result.data.name == null) {
-    }
     DOMSelectors.container.insertAdjacentHTML(
       "beforeend",
-      `<div class="border-8 border-blue-950 rounded-md size-128 ">
-         <h3 class="text-2xl text-center">${boss.name}</h3>
-         <img src="${boss.image}" class="object-contain h-48 w-96  " />
-       </div>`
+      `<div class="border-8 border-blue-950 rounded-md w-96 h-96 max-w-full max-h-full">
+      <h3 class="text-2xl text-center">${boss.name}</h3>
+      <img src="${boss.image}" class="object-contain h-96 w-96 max-h-96" alt="${boss.description}"/>
+  </div>
+  `
     );
   });
 }
