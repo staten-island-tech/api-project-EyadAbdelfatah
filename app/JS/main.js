d152c9daca524f0449f230cd307ecb8cef08f82b
mellowed24 <eyada5@nycstudents.net>
2024-12-01 17:00:49 -0500

    jnd

--- app/JS/main.js
+++ app/JS/main.js
@@ -5,108 +5,90 @@ const DOMSelectors = {
   buttonContain: document.querySelector("#button-container"),
   buttonHOL: document.querySelector("#HOL"),
   HOLcontainer: document.querySelector("#HOLcontainer"),
-  buttons: document.querySelectorAll("button")
+  buttons: document.querySelectorAll("button"),
+  health: document.querySelector("#question"),
+  input: document.querySelector("#search-input"),
+  input_button: document.querySelector("#submit")
 };
-let result, firstCard, secondCard, HOLbuttons;
-
+let result, firstCard, secondCard;
 async function getData() {
   try {
     const response = await fetch(
       "https://eldenring.fanapis.com/api/bosses?limit=100"
     );
     result = await response.json();
+    makeCards(result.data);
   } catch (error) {
-    console.error("Error fetching data:", error);
+    alert("Error fetching data:", error);
   }
 }
-
 getData();
-
-async function makeCards() {
-  await getData();
-  result.data.forEach(boss => {
+function makeCards(arr) {
+  arr.forEach(boss => {
     DOMSelectors.container.insertAdjacentHTML(
       "beforeend",
       `<div class="border-8 border-blue-950 rounded-md w-full h-96 max-h-full">
-      <h3 class="text-2xl text-center">${boss.name}</h3>
+      <h3 class="text-2xl text-center text-white">${boss.name}</h3>
       <img src="${boss.image}" class="object-contain  w-full max-h-full" alt="${boss.description}"/>
   </div>`
     );
   });
 }
-
-makeCards();
 function getCardHTML(card) {
   return `<div class="border-8 border-blue-950 rounded-md w-96 mb-12 h-96" data-health="${card.healthPoints}">
-            <h3 class="text-2xl text-center">${card.name}</h3>
+            <h3 class="text-2xl text-center text-white">${card.name}</h3>
             <img src="${card.image}" class="object-contain w-96" alt="${card.description}"/>
           </div>`;
 }
 const buttonsHTML = `  
   <div class="flex justify-center gap-4 mt-4">
-    <button id="higher" class="btn btn-primary">Higher</button>
-    <button id="lower" class="btn btn-secondary">Lower</button>
+    <button id="higher" class="btn btn-primary text-white">Higher</button>
+    <button id="lower" class="btn btn-secondary text-white">Lower</button>
   </div>`;
 function insertCards(firstCard, secondCard) {
   const firstCardHTML = getCardHTML(firstCard);
   const secondCardHTML = getCardHTML(secondCard);
-
   DOMSelectors.HOLcontainer.insertAdjacentHTML("beforeend", firstCardHTML);
   DOMSelectors.HOLcontainer.insertAdjacentHTML("beforeend", buttonsHTML);
   DOMSelectors.HOLcontainer.insertAdjacentHTML("beforeend", secondCardHTML);
 }
-async function convertToInt() {
-  try {
-    const responses = await fetch(
-      "https://eldenring.fanapis.com/api/bosses?limit=100"
-    );
-    const results = await responses.json();
-    console.log(results);
-
-    results.data.forEach(boss => {
-      if (boss.healthPoints === "???" || !boss.healthPoints) {
-        boss.healthPoints = 0;
-      } else {
-        const NewHealthPoints = String(boss.healthPoints).replace(/\D/g, "");
-        const x = parseInt(NewHealthPoints, 10);
-        boss.healthPoints = x;
-      }
-    });
-
-    const filteredData = results.data.filter(boss => boss.healthPoints !== 0);
-    return filteredData;
-  } catch (error) {}
+function convertToInt() {
+  result.data.forEach(boss => {
+    if (boss.healthPoints === "???" || !boss.healthPoints) {
+      boss.healthPoints = 0;
+    } else {
+      const NewHealthPoints = String(boss.healthPoints).replace(/\D/g, "");
+      const x = parseInt(NewHealthPoints, 10);
+      boss.healthPoints = x;
+    }
+  });
+  const filteredData = result.data.filter(boss => boss.healthPoints !== 0);
+  return filteredData;
 }
-
-async function higherOrLowerSetup() {
+function higherOrLowerSetup() {
   DOMSelectors.container.innerHTML = "";
   DOMSelectors.buttonContain.innerHTML = "";
-
-  const newResult = await convertToInt();
-
+  DOMSelectors.health.insertAdjacentHTML(
+    "beforeend",
+    `<div class="align-center"><h2 class="text-white mb-12">Who has more haelth?</h2></div>`
+  );
+  const newResult = convertToInt();
   const randomnumber = Math.floor(Math.random() * newResult.length);
   const randomnumber2 = Math.floor(Math.random() * newResult.length);
-
   firstCard = newResult[randomnumber];
   secondCard = newResult[randomnumber2];
-
   insertCards(firstCard, secondCard);
-
   higherOrLower(firstCard, secondCard);
 }
-
-async function higherOrLower(firstCard, secondCard) {
-  DOMSelectors.HOLcontainer.addEventListener("click", async function(event) {
+function higherOrLower(firstCard, secondCard) {
+  DOMSelectors.HOLcontainer.addEventListener("click", function(event) {
     let gameOver = false;
     if (!event.target.matches("button")) return;
-
     if (gameOver) return;
-    const newResult = await convertToInt();
+    const newResult = convertToInt();
     DOMSelectors.HOLcontainer.innerHTML = "";
-
     let randomNew = Math.floor(Math.random() * newResult.length);
     let newCard = newResult[randomNew];
-
     if (
       (event.target.id === "higher" &&
         firstCard.healthPoints >= secondCard.healthPoints) ||
@@ -117,11 +99,39 @@ async function higherOrLower(firstCard, secondCard) {
       secondCard = newCard;
       insertCards(firstCard, secondCard);
     } else {
+      DOMSelectors.HOLcontainer.insertAdjacentHTML(
+        "afterbegin",
+        `<button id="back"class="btn btn-secondary">Back</button>`
+      );
       gameOver = true;
     }
   });
 }
-
+async function search() {
+  try {
+    const search_response = await fetch(
+      "https://eldenring.fanapis.com/api/bosses?limit=100"
+    );
+    let search_result = await search_response.json();
+    let user_input = DOMSelectors.input.value.toLowerCase();
+    DOMSelectors.input.value = "";
+    DOMSelectors.container.innerHTML = "";
+    const user__data = search_result.data.filter(boss =>
+      boss.name.toLowerCase().startsWith(user_input)
+    );
+    makeCards(user__data);
+  } catch (error) {
+    alert("Uwhat the bruh");
+  }
+}
 DOMSelectors.buttonHOL.addEventListener("click", function() {
   higherOrLowerSetup();
+});
+DOMSelectors.HOLcontainer.addEventListener("click", function(event) {
+  if (event.target.id === "back") {
+    window.location.reload();
+  }
+});
+DOMSelectors.input_button.addEventListener("click", function() {
+  search();
 });
